9c6690e23cee673553a6bba4ddb5a5dcd5c6a2bd
Zoran Ivanovic <zolevs@gmail.com>
2017-06-25 12:29:14 +0200

    sticky menu

---
+++ sticky menu/plainMenu.js
@@ -0,0 +1,13 @@
+$(document).ready(function){
+	setInterval(checkScroll, 200);
+});
+
+function checkScroll(){
+	var nav = $('.top-nav').first();
+	console.log('nav');
+	if ($(document).scollTop() > 0 && !nav.hasClass('scrolling')) {
+		nav.addClass('scrolling');
+	} else if ($(document).scrollTop() === 0 && nav.hasClass('scrolling')) {
+		nav.removeClass('scrolling');
+	};
+}
