0113d5ffd1f21be87a25a53c9831f7a53e34fabd
Zoran Ivanovic <zolevs@gmail.com>
2017-07-29 23:13:13 +0200

    underline menu

---
+++ menu/jqeury.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$("ul li:nth-child(1)").hover(function(){
+		$("#line").css("margin-left","3px");
+	});
+	$("ul li:nth-child(2)").hover(function(){
+		$("#line").css("margin-left","113px");
+	});
+	$("ul li:nth-child(3)").hover(function(){
+		$("#line").css("margin-left","215px");
+	});
+	$("ul li:nth-child(4)").hover(function(){
+		$("#line").css("margin-left","318px");
+	});
+	$("ul li:nth-child(5)").hover(function(){
+		$("#line").css("margin-left","425px");
+	});
+
+
+})
