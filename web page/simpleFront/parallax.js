53a6fa188a16a2f6cfc39fef6297fb9a35f63bc7
Zoran Ivanovic <zolevs@gmail.com>
2017-08-11 23:12:29 +0200

    parallex effect for box

--- web page/simpleFront/parallax.js
+++ web page/simpleFront/parallax.js
@@ -9,4 +9,6 @@ function parallax(){
 	// console.log(wScroll);
 
 	$('.parallax--bg').css('background-position', 'center '+(wScroll * 0.7)+'px');
+
+	$('.parallax--box').css('top',-5+(wScroll*0.006)+'em');
 }
